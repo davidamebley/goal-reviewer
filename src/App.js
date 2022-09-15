b6c33ba7a323c67be8fd472276cf7ad69dc92b85
David Amebley <davameb@gmail.com>
2022-09-15 09:35:04 +0300

    Add styling to goal list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import './App.css'
+import GoalList from "./components/GoalList/GoalList";
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  return <div className="goals-heading">
+    <h2>My Short-term Goals</h2>
+    <GoalList/>
+  </div>;
 };
 
 export default App;
---
+++ src/components/GoalList/GoalList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './GoalList.css'
+
+const GoalList = () => {
+  return (
+    <ul className='goal-list'>
+        <li>Finnish my MERN stack</li>
+        <li>Refresh on AWS</li>
+        <li>Add more projects</li>
+    </ul>
+  )
+}
+
+export default GoalList
